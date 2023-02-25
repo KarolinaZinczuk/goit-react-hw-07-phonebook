61405b579088565a5251dddf0ae34cbb8b80c4b9
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-25 14:44:42 +0100

    Update ContactsSlice.js

--- src/redux/ContactsSlice.js
+++ src/redux/ContactsSlice.js
@@ -43,7 +43,7 @@ export const contactsSlice = createSlice({
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
-      const index = state.items.findIndex((contact) => contact.id === action.payload);
+      const index = state.items.findIndex((contact) => contact.id === action.payload.id);
       state.items.splice(index, 1);
     },
   }
